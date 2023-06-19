cdad0510ee5b589b2354eda071bbab87231522ee
jiajie22 <38906034+jiajie22@users.noreply.github.com>
2023-06-19 14:24:14 +0800

    Update getGroup.groovy

--- getGroup.groovy
+++ getGroup.groovy
@@ -1,4 +1,15 @@
-def group = "jira-administrators"
-def result = get('/rest/api/3/group?groupname=' + group).asObject(Map).body
-logger.info ("result >> " + result)
-result
+def issueKey = 'CLOUD-1'
+
+def result = get('/rest/api/2/issue/' + issueKey)
+        .header('Content-Type', 'application/json')
+        .asObject(Map)
+if (result.status == 200){
+    return result.body.fields.summary
+} else {
+    return "Failed to find issue: Status: ${result.status} ${result.body}"
+}
+
+// def group = "jira-administrators"
+// def result = get('/rest/api/3/group?groupname=' + group).asObject(Map).body
+// logger.info ("result >> " + result)
+// result
