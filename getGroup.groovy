63bf73712af2f67d9e312b5418c400e637f77a62
jiajie22 <38906034+jiajie22@users.noreply.github.com>
2025-05-06 17:19:45 +0800

    Update getGroup.groovy

--- getGroup.groovy
+++ getGroup.groovy
@@ -1,15 +1,2 @@
-def issueKey = 'SUN-1038'
-
-def result = get('/rest/api/2/issue/' + issueKey)
-        .header('Content-Type', 'application/json')
-        .asObject(Map)
-if (result.status == 200){
-    return result.body.fields.summary
-} else {
-    return "Failed to find issue: Status: ${result.status} ${result.body}"
-}
-
-// def group = "jira-administrators"
-// def result = get('/rest/api/3/group?groupname=' + group).asObject(Map).body
-// logger.info ("result >> " + result)
-// result
+def issue = Issues.getByKey("SUN-1038")
+return "${issue.key}'s summary - ${issue.fields.summary}"
